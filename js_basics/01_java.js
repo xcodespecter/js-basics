2a4a892091b688f4c1dddab41060414d0100db0e
xcodespecter <165976108+xcodespecter@users.noreply.github.com>
2025-04-27 17:42:39 +0000

    02 variabls

--- js_basics/01_java.js
+++ js_basics/01_java.js
@@ -1,1 +1,2 @@
 console.log("souvik");
+console.log("xcode");
---
+++ js_basics/js_variabls.js
@@ -0,0 +1,9 @@
+const accountId=1442222;
+let accountEmail="souvik@google.com";
+var accountPassword="123456";// don't use var variabls use let fot un constable values
+let accountPass="123456";// like thiss both are same but also use let
+accountCity="basirhat";// thiss comant work properly bur don't use this use
+let accountCityy="basirhat";// use thiss
+// for changes non const info 
+accountPass="123456987"
+console.table([accountId,accountEmail,accountPassword,accountPass,accountCity,accountCityy]);
