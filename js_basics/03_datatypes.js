984ad8898842440e5dedeb199e873d7213741c18
xcodespecter <165976108+xcodespecter@users.noreply.github.com>
2025-04-28 08:29:36 +0000

    03_datatypes

---
+++ js_basics/03_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // for newer version of js
+console.log(3+3);console.log("souvik");// also work this type of lines but we don't write like this
+console.log(4+4);// we write like this . also try to write two line separetly
+console.log("xcode");
+// datatypes
+let nameUs ="souvik";
+let agee=19;
+console.table([nameUs]);
+console.log (agee)
+//types of datatypes
+let name ="souvik"// string type data
+let age =19 // integer type data
+let isLogin = true // bullen type data  
+// for know which type of datatypes it is
+console.log(typeof 19);
+console.log(typeof name);
+console.log(typeof isLogin)
+
+console.log(typeof null); // type of null is aobject
+console.log(typeof undefined); // and type of undifined is also undifined (also remember this logic for interview questions)
