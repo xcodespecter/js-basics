4c53b373ffb0c881c0eb2de13bdb2ef5d2d6874e
xcodespecter <165976108+xcodespecter@users.noreply.github.com>
2025-04-28 17:38:30 +0000

    revison 01,02,03 topic

---
+++ js_basics/rev.js
@@ -0,0 +1,18 @@
+console.log("souvik"); // constant type data
+let age=(19); // for change able data
+console.log(age);
+let collegeName=("camellia institute of tecnology");
+let dadName=("kartick Singha");
+console.table([collegeName,dadName]); // console.table is for print multiple data in one comment
+let accountPass=123466;// "" is not use beacuse it is a intiger value
+console.log(accountPass);
+// types of data types
+let name=("souvik"); // string type
+let agee=19; // integer type
+let login = false; // boolean type data
+// how to know which is what kind of data type
+console.log(typeof "name");
+console.log(typeof age);
+console.log(typeof login);
+console.log(typeof null) ; //allways it object type
+console.log(typeof undefined); // it allways be undefined
